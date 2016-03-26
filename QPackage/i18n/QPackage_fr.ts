3cbfbd23bc9e728479ddb70bd81badda40f5d321
sigeal <sigeal@users.noreply.github.com>
2016-03-26 17:15:33 +0100

    Translation update

--- QPackage/i18n/QPackage_fr.ts
+++ QPackage/i18n/QPackage_fr.ts
@@ -2,68 +2,86 @@
 <!DOCTYPE TS>
 <TS version="2.0" language="fr_FR">
 <context>
-    <name>QPackage</name>
-    <message>
-        <location filename="QPackage.py" line="174"/>
-        <source>&amp;QPackage</source>
-        <translation></translation>
-    </message>
-    <message>
-        <location filename="QPackage_dialog.py" line="120"/>
-        <source>QPackage</source>
-        <translation></translation>
-    </message>
-</context>
-<context>
     <name>QPackageDialogBase</name>
     <message>
-        <location filename="QPackage_dialog_base.py" line="69"/>
+        <location filename="QPackage_dialog_base.py" line="119"/>
         <source>QPackage</source>
         <translation>QPackage</translation>
     </message>
     <message>
-        <location filename="QPackage_dialog_base.py" line="70"/>
+        <location filename="QPackage_dialog_base.py" line="120"/>
+        <source>Load layers of the current project</source>
+        <translation>Charger les couches du projet courant</translation>
+    </message>
+    <message>
+        <location filename="QPackage_dialog_base.py" line="121"/>
         <source>Destination folder</source>
         <translation>Répertoire de destination</translation>
     </message>
     <message>
-        <location filename="QPackage_dialog_base.py" line="71"/>
-        <source>Load layers of the current project</source>
-        <translation>Lecture des couches du projet</translation>
+        <location filename="QPackage_dialog_base.py" line="122"/>
+        <source>Project name : </source>
+        <translation>Nom du projet : </translation>
     </message>
     <message>
-        <location filename="QPackage_dialog_base.py" line="72"/>
+        <location filename="QPackage_dialog_base.py" line="123"/>
+        <source>Choose the CRS : </source>
+        <translation>Choisir le SCR : </translation>
+    </message>
+    <message>
+        <location filename="QPackage_dialog_base.py" line="124"/>
         <source>Copy the layers</source>
         <translation>Copier les couches</translation>
-    </message>
-    <message>
-        <location filename="QPackage_dialog_base.py" line="73"/>
-        <source>Choose the CRS</source>
-        <translation>Choisir le SCR</translation>
-    </message>
-</context>
-<context>
-    <name>choosedestination</name>
-    <message>
-        <location filename="QPackage_dialog.py" line="99"/>
-        <source>You must choose the destination directory</source>
-        <translation>Vous devez sélectionner un répertoire de destination</translation>
-    </message>
-</context>
-<context>
-    <name>mustclose</name>
-    <message>
-        <location filename="QPackage_dialog.py" line="120"/>
-        <source>You have to re-open project to check changes</source>
-        <translation>Vous devez ré-ouvrir le projet pour vérifier les modifications</translation>
     </message>
 </context>
 <context>
     <name>select destination</name>
     <message>
-        <location filename="QPackage_dialog.py" line="49"/>
+        <location filename="QPackage_dialog.py" line="65"/>
         <source>Select directory to Fling from...</source>
         <translation>Vous devez sélectionner un répertoire de destination...</translation>
+    </message>
+</context>
+<context>
+    <name>choosedestination</name>
+    <message>
+        <location filename="QPackage_dialog.py" line="138"/>
+        <source>You must choose the destination directory</source>
+        <translation>Vous devez sélectionner un répertoire de destination.</translation>
+    </message>
+</context>
+<context>
+    <name>choosedestination</name>
+    <message>
+        <location filename="QPackage_dialog.py" line="154"/>
+        <source>You must choose the destination directory</source>
+        <translation>Vous devez sélectionner un répertoire de destination.</translation>
+    </message>
+</context>
+<context>
+    <name>mustclose</name>
+    <message>
+        <location filename="QPackage_dialog.py" line="228"/>
+        <source>Layers export completed. You can to re-open project to check changes.</source>
+        <translation>Export des couches terminé. Vous pouvez ré-ouvrir le projet pour vérifier les modifications.</translation>
+    </message>
+</context>
+<context>
+    <name>select crs</name>
+    <message>
+        <location filename="QPackage_dialog_base.py" line="73"/>
+        <source>- Select CRS -</source>
+        <translation>- Sélectionner le SCR -</translation>
+    </message>
+    <message>
+        <location filename="QPackage_dialog_base.py" line="122"/>
+        <source>- Select CRS -</source>
+        <translation>- Sélectionner le SCR -</translation>
+    </message>
+    <message>
+        <location filename="QPackage_dialog_base.py" line="201"/>
+        <source>- Select CRS -</source>
+        <translation>- Sélectionner le SCR -</translation>
     </message>
 </context>
 </TS>
